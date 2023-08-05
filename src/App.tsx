56a69abd155706b944db82bd20d8d9de5d523989
rkodirkhonov <kodirkhonov99@gmail.com>
2023-08-05 19:20:19 +0900

    finished the initial stage of the project totally

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,14 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 import { useAppDispatch, useAppSelector } from "./app/hooks";
 import { incremented } from "./features/counter/counter-slice";
+import { useFetchBreedsQuery } from "./features/dog/dogs-api-slice";
+import { useState } from "react";
 
 function App() {
-  const count = useAppSelector((state) => state.cunter.value);
+  const count = useAppSelector((state) => state.counter.value);
   const dispatch = useAppDispatch();
+  const [numDogs, setNumDogs] = useState(10);
+  const { data = [], isFetching } = useFetchBreedsQuery(numDogs);
 
   function handleClick() {
     dispatch(incremented());
@@ -27,6 +31,44 @@ function App() {
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={handleClick}>count is {count}</button>
+        <div>
+          <select
+            value={numDogs}
+            onChange={(e) => setNumDogs(Number(e.target.value))}
+          >
+            <option value={"5"}>5</option>
+            <option value={"10"}>10</option>
+            <option value={"15"}>15</option>
+            <option value={"20"}>20</option>
+          </select>
+        </div>
+        <div>
+          <p>Number of dogs fetched: {data.length}</p>
+          <table>
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Picture</th>
+              </tr>
+            </thead>
+            <tbody>
+              {data.map((breed) => (
+                <tr key={breed.id}>
+                  <td>{breed.name}</td>
+                  <td>
+                    {
+                      <img
+                        src={breed.image.url}
+                        alt={breed.name}
+                        height={250}
+                      />
+                    }
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
